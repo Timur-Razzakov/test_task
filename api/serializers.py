0f6d221c217e289e834a69bbd1254542cf8d1543
Timur-Razzakov <razzakov334@gmail.com>
2023-10-24 22:37:37 +0500

    added UserListSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,21 +2,20 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.validators import ValidationError
 
-from .models import User
+from .models import MyUser
 
 
 class SignUpSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(max_length=80)
     username = serializers.CharField(max_length=45)
+    email = serializers.CharField(max_length=80)
     password = serializers.CharField(min_length=8, write_only=True)
 
     class Meta:
-        model = User
+        model = MyUser
         fields = ["email", "username", "password"]
 
     def validate(self, attrs):
-
-        email_exists = User.objects.filter(email=attrs["email"]).exists()
+        email_exists = MyUser.objects.filter(email=attrs["email"]).exists()
 
         if email_exists:
             raise ValidationError("Email has already been used")
@@ -26,12 +25,19 @@ class SignUpSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop("password")
 
-        user = super().create(validated_data)
-
+        user = MyUser.objects.create_user(**validated_data)
         user.set_password(password)
-
         user.save()
 
         Token.objects.create(user=user)
 
         return user
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    """Для вывода пользователей"""
+
+    class Meta:
+        model = MyUser
+        fields = ["id", "email", "username"]
+

54fdf342e47044ec33cebea2c1136c2bbfb4979b
Timur-Razzakov <razzakov334@gmail.com>
2023-10-24 22:38:16 +0500

    added update,del,search user

--- api/views.py
+++ api/views.py
@@ -1,17 +1,21 @@
-from django.contrib.auth import authenticate
-from django.shortcuts import render
+from django.contrib.auth import authenticate, get_user_model, login
 from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny,
+    IsAuthenticatedOrReadOnly,
+    IsAdminUser,
+)
+from .serializers import SignUpSerializer, UserListSerializer
+from .tokens import create_jwt_pair_for_user
 
-from .serializers import SignUpSerializer
+User = get_user_model()
 
-
-# from .tokens import create_jwt_pair_for_user
 
 class SignUpView(generics.GenericAPIView):
-    """Регистрируем пользователя"""
     serializer_class = SignUpSerializer
     permission_classes = []
 
@@ -26,7 +30,6 @@ class SignUpView(generics.GenericAPIView):
             response = {"message": "User Created Successfully", "data": serializer.data}
 
             return Response(data=response, status=status.HTTP_201_CREATED)
-
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -38,11 +41,10 @@ class LoginView(APIView):
         password = request.data.get("password")
 
         user = authenticate(email=email, password=password)
-
+        login(request, user)
         if user is not None:
-
+            # создаём токен для авторизации
             tokens = create_jwt_pair_for_user(user)
-
             response = {"message": "Login Successfull", "tokens": tokens}
             return Response(data=response, status=status.HTTP_200_OK)
 
@@ -50,6 +52,38 @@ class LoginView(APIView):
             return Response(data={"message": "Invalid email or password"})
 
     def get(self, request: Request):
+        # выводим информацию об авторизованном пользователе
         content = {"user": str(request.user), "auth": str(request.auth)}
 
         return Response(data=content, status=status.HTTP_200_OK)
+
+
+class ListUsersView(generics.ListAPIView):
+    """Выводим пользователей"""
+    serializer_class = UserListSerializer
+    permission_classes = []  # IsAuthenticated
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+class UserView(generics.RetrieveUpdateDestroyAPIView):
+    """Редактирование пользователя"""
+    serializer_class = SignUpSerializer
+    permission_classes = []  # IsAuthenticated
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+class UserSearchView(APIView):
+    """Поиск по имени"""
+    permission_classes = []
+
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+            return Response({"username": user.username, "email": user.email}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"message": "Пользователь не найден"}, status=status.HTTP_404_NOT_FOUND)
+

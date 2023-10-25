cede5dcde5b2889b16233b8524de5666bcd1e52d
Timur-Razzakov <razzakov334@gmail.com>
2023-10-25 18:44:46 +0500

    updated url for new view

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from . import views
 
+# для view на  SQL запросах
 urlpatterns = [
-    path("registratin/", views.SignUpView.as_view(), name="registration"),
+    path("registratin/", views.CreateUserView.as_view(), name="registration"),
     path("authorization/", views.LoginView.as_view(), name="authorization"),
-    path("users_list/", views.ListUsersView.as_view(), name="users_list"),
-    path("users_list/<int:pk>/", views.UserView.as_view(), name="users"),
-    path("search/<str:username>/", views.UserSearchView.as_view(), name="search"),
+    path("users_list/", views.GetAllUsersView.as_view(), name="users_list"),
+    path("users_list/<int:pk>/", views.GetUserView.as_view(), name="users"),
+    path("update_user/<int:pk>/", views.UpdateUserView.as_view(), name="update_user"),
+    path("delete_user/<int:pk>/", views.DeleteUserView.as_view(), name="delete_user"),
+    path("search_user/<str:username>/", views.SearchUserView.as_view(), name="search_user"),
 
 ]

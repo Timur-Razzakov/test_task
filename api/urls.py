08d6255870d58995543137bb1fe3c7e32638ce10
Timur-Razzakov <razzakov334@gmail.com>
2023-10-25 15:12:43 +0500

    change name for show all users

--- api/urls.py
+++ api/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", views.LoginView.as_view(), name="login"),
-    path("all_users/", views.ListUsersView.as_view(), name="all_users"),
-    path("all_users/<int:pk>/", views.UserView.as_view(), name="edit_user"),
+    path("user_list/", views.ListUsersView.as_view(), name="uuser_list"),
+    path("user_list/<int:pk>/", views.UserView.as_view(), name="edit_user"),
     path("search/<str:username>/", views.UserSearchView.as_view(), name="search"),
 
 ]

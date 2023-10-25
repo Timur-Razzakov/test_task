1aa569baf21775dc8fec196fedbda7b8018274c9
Timur-Razzakov <razzakov334@gmail.com>
2023-10-25 16:56:05 +0500

    changed urls name

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-
 from . import views
 
 urlpatterns = [
-    path("signup/", views.SignUpView.as_view(), name="signup"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("user_list/", views.ListUsersView.as_view(), name="uuser_list"),
-    path("user_list/<int:pk>/", views.UserView.as_view(), name="edit_user"),
+    path("registratin/", views.SignUpView.as_view(), name="registration"),
+    path("authorization/", views.LoginView.as_view(), name="authorization"),
+    path("users_list/", views.ListUsersView.as_view(), name="users_list"),
+    path("users_list/<int:pk>/", views.UserView.as_view(), name="users"),
     path("search/<str:username>/", views.UserSearchView.as_view(), name="search"),
 
 ]

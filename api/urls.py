4522c8e6a754ef34155d198d74e96ffebe98bcf5
Timur-Razzakov <razzakov334@gmail.com>
2023-10-25 14:44:23 +0500

    moved token (create, refresh etc..) to rest_full_api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+
 
 from . import views
 
@@ -13,8 +9,5 @@ urlpatterns = [
     path("all_users/", views.ListUsersView.as_view(), name="all_users"),
     path("all_users/<int:pk>/", views.UserView.as_view(), name="edit_user"),
     path("search/<str:username>/", views.UserSearchView.as_view(), name="search"),
-    # JWT
-    path("jwt/create/", TokenObtainPairView.as_view(), name="jwt_create"),
-    path("jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
 ]

0a7579bb769c4165eba2d3a9079e352b9ba30646
Timur-Razzakov <razzakov334@gmail.com>
2023-10-25 14:42:40 +0500

    added serializator for UserSearchView and ListUsersView for fix problem with swagger

--- api/views.py
+++ api/views.py
@@ -1,17 +1,14 @@
 import logging
 
 from django.contrib.auth import authenticate, get_user_model, login
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import (
-    IsAuthenticated,
-    AllowAny,
-    IsAuthenticatedOrReadOnly,
-    IsAdminUser,
-)
-from .serializers import SignUpSerializer, UserListSerializer
+    IsAuthenticated)
+from .serializers import SignUpSerializer, UserListSerializer, LoginSerializer
 from .tokens import create_jwt_pair_for_user
 
 User = get_user_model()
@@ -19,11 +16,8 @@ User = get_user_model()
 # Создайте логгер
 logger = logging.getLogger(__name__)
 
+#  python manage.py spectacular --file schema.yml
 
-# logger.error('The username is empty:  %s', username)  # Логирование ошибки
-
-# TODO: logs
-# TODO: swagger
 # TODO: filter and sort user
 # TODO: Check RESTFULL API
 # TODO: Readme
@@ -52,24 +46,31 @@ class SignUpView(generics.GenericAPIView):
 class LoginView(APIView):
     """Получаем токен, для авторизации"""
     permission_classes = []
+    serializer_class = LoginSerializer
 
-    def post(self, request: Request):
-        email = request.data.get("email")
-        password = request.data.get("password")
-
-        user = authenticate(email=email, password=password)
-        if user is not None:
-            # создаём токен для авторизации
-            tokens = create_jwt_pair_for_user(user)
-            logger.info('Successful created token for user:  %s', user)
-            response = {"message": "Login Successfull", "tokens": tokens}
-            return Response(data=response, status=status.HTTP_200_OK)
-
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            email = serializer.validated_data.get("email")
+            password = serializer.validated_data.get("password")
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                # создаём токен для авторизации
+                tokens = create_jwt_pair_for_user(user)
+                logger.info('Successful created token for user:  %s', user)
+                response = {"message": "Login Successfully", "tokens": tokens}
+                return Response(data=response, status=status.HTTP_200_OK)
+            else:
+                logger.error('Invalid email or password for user:  %s', email)
+                return Response(data={"message": "Invalid email or password"},
+                                status=status.HTTP_401_UNAUTHORIZED)
         else:
-            logger.error('Invalid email or password:  %s', user)
-            return Response(data={"message": "Invalid email or password"})
+            logger.error('Invalid data received for login')
+            return Response(data={"message": "Invalid data received for login"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request: Request):
+    def get(self, request):
         # выводим информацию об авторизованном пользователе
         content = {"user": str(request.user), "auth": str(request.auth)}
 
@@ -98,6 +99,8 @@ class UserSearchView(APIView):
     """Поиск по имени"""
     permission_classes = [IsAuthenticated]
 
+    # для избавления от ошибки 'unable to guess serializer' при использовании swagger-ра
+    @extend_schema(responses=LoginSerializer)
     def get(self, request, username):
         try:
             user = User.objects.get(username=username)

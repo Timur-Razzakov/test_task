a578484711088fc9fed8ec168a7cc9e04f144b5e
Timur-Razzakov <razzakov334@gmail.com>
2023-10-25 13:54:44 +0500

    added loggers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth import authenticate, get_user_model, login
 from rest_framework import generics, status
 from rest_framework.request import Request
@@ -14,8 +16,21 @@ from .tokens import create_jwt_pair_for_user
 
 User = get_user_model()
 
+# Создайте логгер
+logger = logging.getLogger(__name__)
+
+
+# logger.error('The username is empty:  %s', username)  # Логирование ошибки
+
+# TODO: logs
+# TODO: swagger
+# TODO: filter and sort user
+# TODO: Check RESTFULL API
+# TODO: Readme
+# TODO: PEP8 check
 
 class SignUpView(generics.GenericAPIView):
+    """Проводим регистрацию"""
     serializer_class = SignUpSerializer
     permission_classes = []
 
@@ -26,14 +41,16 @@ class SignUpView(generics.GenericAPIView):
 
         if serializer.is_valid():
             serializer.save()
-
+            logger.info('Successful created user:  %s', serializer.data)
             response = {"message": "User Created Successfully", "data": serializer.data}
 
             return Response(data=response, status=status.HTTP_201_CREATED)
+        logger.error('serializer is not valid:  %s', serializer.data)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
+    """Получаем токен, для авторизации"""
     permission_classes = []
 
     def post(self, request: Request):
@@ -44,10 +61,12 @@ class LoginView(APIView):
         if user is not None:
             # создаём токен для авторизации
             tokens = create_jwt_pair_for_user(user)
+            logger.info('Successful created token for user:  %s', user)
             response = {"message": "Login Successfull", "tokens": tokens}
             return Response(data=response, status=status.HTTP_200_OK)
 
         else:
+            logger.error('Invalid email or password:  %s', user)
             return Response(data={"message": "Invalid email or password"})
 
     def get(self, request: Request):
@@ -69,7 +88,7 @@ class ListUsersView(generics.ListAPIView):
 class UserView(generics.RetrieveUpdateDestroyAPIView):
     """Редактирование пользователя"""
     serializer_class = SignUpSerializer
-    permission_classes = [IsAuthenticated]  #
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return User.objects.all()
@@ -84,4 +103,5 @@ class UserSearchView(APIView):
             user = User.objects.get(username=username)
             return Response({"username": user.username, "email": user.email}, status=status.HTTP_200_OK)
         except User.DoesNotExist:
+            logger.error('The user is empty: UserSearchView %s ', user.username)
             return Response({"message": "Пользователь не найден"}, status=status.HTTP_404_NOT_FOUND)
--- rest_full_api/settings.py
+++ rest_full_api/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-from datetime import timedelta
+from datetime import timedelta, datetime
 from pathlib import Path
 
 import dotenv
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
 
 # Application definition
 
@@ -75,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rest_full_api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -116,7 +112,6 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -145,5 +140,36 @@ SIMPLE_JWT = {
     "SIGNING_KEY": SECRET_KEY,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
-
+# кастомный user
 AUTH_USER_MODEL = 'api.MyUser'
+
+# Конфигурация логирования
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '{}/logs/my_log_{}.log'.format(BASE_DIR, datetime.now().strftime('%Y-%m-%d')),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+    'formatters': {
+        'standard': {
+            'format': '{asctime} - {levelname}  {module}.{funcName}:{lineno} - {message}',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+            'style': '{',
+        },
+    },
+}
